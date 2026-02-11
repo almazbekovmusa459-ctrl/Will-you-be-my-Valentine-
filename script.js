2f414f2f3e52591626b45b7bcf8141762d8858d3
almazbekovmusa459-ctrl <almazbekovmusa459@gmail.com>
2026-02-11 16:10:06 +0600

    Update script.js

--- script.js
+++ script.js
@@ -53,17 +53,17 @@
     }
 })();
 */
-const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+const messages = ;[
+    "Ты уверен(а)?",
+"Точно уверен(а)??",
+"Ты абсолютно уверен(а)?" ,
+"Пуки, ну пожа-а-алуйста…",
+"Просто подумай об этом!",
+"Если ты скажешь “нет”, мне будет очень грустно…"
+"Мне будет очень-очень грустно…",
+"Мне будет ооочень-очень-очень грустно…",
+"Ладно, всё, я перестаю спрашивать…",
+"Шучу, пожалуйста, скажи “да”! ❤️",
 ];
 
 let messageIndex = 0;
